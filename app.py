839a518056e4e0a0228ebfc4be71260e25c002c5
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-12-29 11:56:04 +0100

    spaces

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-    
-@app.route('/mypage/me')
-def me():  
+
+@app.route("/mypage/me")
+def me():
     return render_template("me.html")
 
-@app.route('/mypage/contact', methods=['POST', 'GET'])
-def contact():  
-    if request.method == 'POST': 
-        my_text = request.form.get('my_text') 
+
+@app.route("/mypage/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        my_text = request.form.get("my_text")
         print(my_text)
         return redirect("/mypage/contact")
     else:
         return render_template("contact.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
